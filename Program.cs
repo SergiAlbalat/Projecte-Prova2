221ffc535ea0d3655ce621eceb955554f1b91395
SergiAlbalat <sergi.albalat.7e8@itb.cat>
2024-12-05 13:12:18 +0100

    Exercici3 completat Closes #5

--- Program.cs
+++ Program.cs
@@ -1,40 +1,77 @@
 ﻿using System;
-using Prova;
 namespace ProvaT2
 {
-    //Precon: L'usuari escriu quin numero vol buscar en l'array
-    //Postcon: L'usuari sap si el numero esta en l'array o no
-    public class Program
+    //PRE: rep 3 números
+    public class validate_num
     {
+        public static bool InRange(int num, int min, int max)
+        {
+            return num >= min && num <= max;
+        }
+
+        public static bool MesLlarg(int mes)
+        {
+            return mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12;
+        }
+
+        public static bool Bisiesto(int any)
+        {
+            return (any % 400 == 0) || ((any % 4 == 0) && (any % 100 != 0));
+        }
+
         public static void Main()
         {
-            const string Msg1 = "Write what number do you want to search in the array";
-            const string Msg2 = "The number is not in the array";
-            const string Msg3 = "The number is in the array";
-            const string FormatError = "The format of the number is incorrect";
-            int[] array = { 10, 4, 6, 4, 8, -13, 2, 3 };
-            int key;
+            const string Msg1 = "Introdueix el dia, mes i any";
+            const string Msg2 = "El dia no esta dins del rang";
+            const string Msg3 = "El mes no esta dins del rang";
+            const string Msg4 = "La data es correcta";
+            const string FormatError = "El format de la dada es incorrecte";
+            int dia, mes, any;
+            bool validat = true;
+
             Console.WriteLine(Msg1);
             try
             {
-                key = int.Parse(Console.ReadLine());
-                SecondSort.Order(ref array, 0, array.Length - 1);
-                if(SearchClass.BinarySearch(array, 0, array.Length-1, key) == -1)
+                dia = int.Parse(Console.ReadLine());
+                mes = int.Parse(Console.ReadLine());
+                any = int.Parse(Console.ReadLine());
+                if (InRange(mes, 1, 12))
                 {
-                    Console.WriteLine(Msg2);
+                    if (MesLlarg(mes) && !InRange(dia, 1, 31))
+                    {
+                        Console.WriteLine(Msg2);
+                        validat = false;
+                    }
+                    else if (mes == 2 && !InRange(dia, 1, 28) && !Bisiesto(any))
+                    {
+                        Console.WriteLine(Msg2);
+                        validat = false;
+                    }
+                    else if (mes == 2 && !InRange(dia, 1, 29) && Bisiesto(any))
+                    {
+                        Console.WriteLine(Msg2);
+                        validat = false;
+                    }
+                    else if (!InRange(dia, 1, 30))
+                    {
+                        Console.WriteLine(Msg2);
+                        validat = false;
+                    }
                 }
                 else
                 {
                     Console.WriteLine(Msg3);
+                    validat = false;
                 }
-                for(int i = 0; i < array.Length; i++)
+                if (validat)
                 {
-                    Console.WriteLine(array[i]);
+                    Console.WriteLine(Msg4);
                 }
-            }catch (FormatException)
+            }
+            catch (FormatException)
             {
                 Console.WriteLine(FormatError);
             }
         }
-    }
+    }//POST: retorna si els números (dd, mm, yyyy) estan dins del rang de data corresponent
 }
